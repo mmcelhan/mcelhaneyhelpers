1f30a0d41c40eb3f38242d74bd47422706d6ab78
mmcelhan <matthewmcelhaney@gmail.com>
2022-01-03 13:52:19 -0700

    push

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mcelhaneyhelpers',
-      version='0.1',
+      version='0.2',
       description='collection of helper and logger functions',
       url='https://lamplightlab.com',
       author='Matthew McElhaney',
